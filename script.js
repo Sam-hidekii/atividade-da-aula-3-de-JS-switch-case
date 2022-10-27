de53d61a048d994f859afe06e9228ce4bdd63235
Sam-hidekii <106408406+Sam-hidekii@users.noreply.github.com>
2022-10-26 21:09:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+/*Aries - de 21 março a 20 abril
+
+Touro -  de 21 abril a 20 maio
+
+Gemeos - de 21 maio a 20 junho
+
+Cancer - de 21 junho a 22 julho
+
+Leao - de 23 julho a 22 agosto
+
+Virgem - de 23 agosto a 22 setembro
+Caso não tenha o signo - Signo não registrado*/
+
+var signo = prompt('Digita seu signo ae carai:')
+
+switch (signo) {
+    case 'aries':
+        console.log('de 21 março a 20 abril')
+        break;
+    case 'touro':
+        console.log('de 21 abril a 20 maio')
+            break;
+    case 'gemeos':
+        console.log('de 21 maio a 20 junho')
+                break;
+    case 'cancer':
+        console.log('de 21 junho a 22 julho')
+                    break;
+    case 'leao':
+        console.log('de 23 julho a 22 agosto')
+                        break;
+     case 'virgem':
+        console.log('de 23 agosto a 22 setembro')
+                            break;
+                
+    default:
+        console.log('Signo não registrado')
+        break;
+}
